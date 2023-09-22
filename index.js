68bb1e76328685a73f8bfb12f39b06406a243fa9
Ruth <ruro122020@gmail.com>
2023-09-22 19:29:26 -0400

    create myReduce function

--- index.js
+++ index.js
@@ -27,3 +27,29 @@ function myMap(collection, callback) {
     }
     return mappedArray
 }
+
+function myReduce(collection, callback, acc){
+    let newArray;
+    if (!Array.isArray(collection)) {
+        newArray = Object.values(collection)
+    } else {
+        newArray = [...collection]
+    }
+
+    let total;
+    if(acc === undefined){
+        total = newArray[0]
+        newArray.shift()
+    }else{
+        total = acc
+    }
+
+    for(let i=0; i < newArray.length; i++){ 
+        total = callback(total, newArray[i], newArray)
+    }
+    return total
+}
+
+console.log(myReduce([1, 2, 3], function(acc, val, collection) { return acc + val; }, 10));
+
+console.log(myReduce({one: 1, two: 2, three: 3}, function(acc, val, collection) { return acc + val; }));
