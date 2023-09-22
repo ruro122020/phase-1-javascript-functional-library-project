f500be8c865b05568924cf9a7ac219181c69b869
Ruth <ruro122020@gmail.com>
2023-09-22 17:10:42 -0400

    create myMap function

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-function myEach(array, callback){
+function myEach(array, callback) {
     let newArray
-    if(!Array.isArray(array)){
+    if (!Array.isArray(array)) {
         newArray = Object.values(array)
-    }else{
+    } else {
         newArray = array
     }
 
-    for(let element of newArray){
+    for (let element of newArray) {
         callback(element)
     }
     return array
 }
 
 
+function myMap(collection, callback) {
+    let newArray;
+    if (!Array.isArray(collection)) {
+        newArray = Object.values(collection)
+    } else {
+        newArray = collection
+    }
+
+    const mappedArray = []
+    for(let i=0; i < newArray.length; i++){
+        mappedArray.push(callback(newArray[i]))
+    }
+    return mappedArray
+}
