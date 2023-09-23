bb9e81770c02c6369a46683585f163958e7bf965
Ruth <ruro122020@gmail.com>
2023-09-23 00:40:59 -0400

    create myLast function

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ function myMap(collection, callback) {
     }
 
     const mappedArray = []
-    for(let i=0; i < newArray.length; i++){
+    for (let i = 0; i < newArray.length; i++) {
         mappedArray.push(callback(newArray[i]))
     }
     return mappedArray
 }
 
-function myReduce(collection, callback, acc){
+function myReduce(collection, callback, acc) {
     let newArray;
     if (!Array.isArray(collection)) {
         newArray = Object.values(collection)
@@ -37,20 +37,20 @@ function myReduce(collection, callback, acc){
     }
 
     let total;
-    if(acc === undefined){
+    if (acc === undefined) {
         total = newArray[0]
         newArray.shift()
-    }else{
+    } else {
         total = acc
     }
 
-    for(let i=0; i < newArray.length; i++){ 
+    for (let i = 0; i < newArray.length; i++) {
         total = callback(total, newArray[i], newArray)
     }
     return total
 }
 
-function myFind(collection, predicate){
+function myFind(collection, predicate) {
     let newArray;
     if (!Array.isArray(collection)) {
         newArray = Object.values(collection)
@@ -58,50 +58,15 @@ function myFind(collection, predicate){
         newArray = [...collection]
     }
 
-    for(let element of newArray){
-        if(predicate(element)){
+    for (let element of newArray) {
+        if (predicate(element)) {
             return element
         }
     }
 
 }
-/*
-Parameter(s):
 
-a collection (either an object or an array)
-a predicate (a callback function that returns true or false)
-Return value:
-
-An array
-Behavior:
-
-Looks through each value in the collection, returning an array of all the values that pass a truth test (predicate). 
-If no matching values are found, it should return an empty array.
-
-Example function call:
-
-myFilter([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
-=> [2, 4, 6]
-
-myFilter({one: 1, three: 3, five: 5}, function(num){ return num % 2 == 0; })
-=> []
-
-This function should iterate through the elements of an array and return all the elements that pass the truth test.
-if there is no match retrun an array. if there is a match return an array with all the elements that passed the truth test.
-
-psudo code
-check to see what kind of object the collection is. If its an array make a copy of it, if it an object turn it to an array using its
-value as the elements in the array
-initialize an empty array
-iterate through the collection array
-    if predicate(element)
-        push the element to the empty array
-    
-return the array
-
-*/
-
-function myFilter(collection, predicate){
+function myFilter(collection, predicate) {
     let newArray;
     if (!Array.isArray(collection)) {
         newArray = Object.values(collection)
@@ -109,48 +74,125 @@ function myFilter(collection, predicate){
         newArray = [...collection]
     }
     let matchedElements = []
-    for(let element of newArray){
-        if(predicate(element)){
+    for (let element of newArray) {
+        if (predicate(element)) {
             matchedElements.push(element)
         }
     }
     return matchedElements
 }
 
-/*
-Parameter(s):
 
-a collection (either an object or an array)
-Return value:
 
-An integer
-Behavior:
-
-Return the number of values in the collection.
-
-Example function calls:
-
-mySize({one: 1, two: 2, three: 3});
-=> 3
-
-mySize([]);
-=> 0
-
-This function should return the size of the array. It should count how many elements the array has and return that number as
-an integer. 
-
-psudo code 
-check to see if the collection is an object or array. if its an array make a copy of it, if its an object convert it to an 
-array and use the values as the elements of the array
-*/
-
-function mySize(collection){
+function mySize(collection) {
     let newArray;
-    if(!Array.isArray(collection)){
+    if (!Array.isArray(collection)) {
         newArray = Object.values(collection)
-    }else{
+    } else {
         newArray = [...collection]
     }
 
     return newArray.length
+}
+
+/*
+Parameter(s):
+
+an array
+an integer (optional)
+Return value:
+
+A single element OR an array
+Behavior:
+
+Returns the first element of an array. Passing n will return the first n elements of the array.
+
+Example function calls:
+
+myFirst([5, 4, 3, 2, 1]);
+=> 5
+
+myFirst([5, 4, 3, 2, 1], 3);
+=> [5, 4, 3]
+
+This function will take in an array and an integer as an optional argument.
+This function should return a single element OR an array.
+What this means is that if an integer is passed as the second argument, return that amount of elements from the passed in array
+if there is no integer passed return the first element of the array
+
+psudo code
+check to see if the array is an object or an array. if object convert it to an array with the values as its elements.
+if array make a copy of the array.
+
+check to see if an integer was passed
+    initialize empty array
+    if integer exist
+        iterate through the array n amount of times
+            push the number to the empty array 
+    otherwise return the first number of the array
+
+*/
+function myFirst(collection, n) {
+    let newArray;
+    if (!Array.isArray(collection)) {
+        newArray = Object.values(collection)
+    } else {
+        newArray = [...collection]
+    }
+
+    if (n) {
+        let nthNumbers = []
+        for (let i = 0; i < n; i++) {
+            nthNumbers.push(newArray[i])
+        }
+        return nthNumbers
+    }else{
+       return newArray[0]
+    }
+}
+
+/*
+Parameter(s):
+
+an array
+an integer (optional)
+Return value:
+
+A single element OR an array
+Behavior:
+
+Returns the last element of an array. Passing n will return the last n elements of the array.
+
+Example function calls:
+
+myLast([5, 4, 3, 2, 1]);
+=> 1
+
+myLast([5, 4, 3, 2, 1], 3);
+=> [3, 2, 1]
+
+This function should take in an array and an optional number.
+It should return the last nth number of the array if a number is passed
+It should return the last number of the array if a number is NOT passed
+
+psudo code
+first check if the array is an object, if object conver to array with values as its elements. if array make copy of array.
+
+if 'n' number was passed 
+        slice the array by subtracting the length of the array by the 'n' number
+otherwise return the last element of the array
+*/
+function myLast(collection, n){
+    let newArray;
+    if (!Array.isArray(collection)) {
+        newArray = Object.values(collection)
+    } else {
+        newArray = [...collection]
+    }
+
+    if(n){
+       return newArray.slice(newArray.length - n)
+    }else{
+        return newArray[newArray.length - 1]
+    }
 }
