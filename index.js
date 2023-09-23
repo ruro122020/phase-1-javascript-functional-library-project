70d6d21ce2695716f4159531d4a06ea4189d49d5
Ruth <ruro122020@gmail.com>
2023-09-23 00:06:44 -0400

    create myFilter function

--- index.js
+++ index.js
@@ -50,20 +50,6 @@ function myReduce(collection, callback, acc){
     return total
 }
 
-/*
-Looks through each value in the collection, returning the first one that passes a truth test (predicate) 
-or undefined if no value passes the test.The function should return as soon as it finds an acceptable element,
-without traversing the rest of the collection.
-
-The function should iterate through each element in the array. 
-Return the first element that passes the truth test(condition) and 
-if no value passes the test return undefined. 
-
-psudo code 
-iterate through the newArray 
-    if callback(element)
-        return element
-*/
 function myFind(collection, predicate){
     let newArray;
     if (!Array.isArray(collection)) {
@@ -78,4 +64,55 @@ function myFind(collection, predicate){
         }
     }
 
+}
+/*
+Parameter(s):
+
+a collection (either an object or an array)
+a predicate (a callback function that returns true or false)
+Return value:
+
+An array
+Behavior:
+
+Looks through each value in the collection, returning an array of all the values that pass a truth test (predicate). 
+If no matching values are found, it should return an empty array.
+
+Example function call:
+
+myFilter([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
+=> [2, 4, 6]
+
+myFilter({one: 1, three: 3, five: 5}, function(num){ return num % 2 == 0; })
+=> []
+
+This function should iterate through the elements of an array and return all the elements that pass the truth test.
+if there is no match retrun an array. if there is a match return an array with all the elements that passed the truth test.
+
+psudo code
+check to see what kind of object the collection is. If its an array make a copy of it, if it an object turn it to an array using its
+value as the elements in the array
+initialize an empty array
+iterate through the collection array
+    if predicate(element)
+        push the element to the empty array
+    
+return the array
+
+*/
+
+function myFilter(collection, predicate){
+    let newArray;
+    if (!Array.isArray(collection)) {
+        newArray = Object.values(collection)
+    } else {
+        newArray = [...collection]
+    }
+    let matchedElements = []
+    for(let element of newArray){
+        if(predicate(element)){
+            matchedElements.push(element)
+        }
+    }
+    return matchedElements
 }
